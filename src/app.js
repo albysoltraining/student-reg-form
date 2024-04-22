bae8330fece6563b66a2d77c05b81ddbaef80ef3
albysoltraining <albysoltraining@gmail.com>
2024-04-21 22:44:36 -0700

    changed text

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'html')));
 app.use(session({
-    secret: 'my-secret-key',
+    secret: 'my-secret-key-2',
     resave: true,
     saveUninitialized: true
 }));
